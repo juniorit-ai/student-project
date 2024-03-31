8f7591bcccab200a3adae2cab3c7d00e4a9e3068
ezhomelabs <haiwei.he@gmail.com>
2024-03-31 12:30:10 +1100

    add session-75311

---
+++ session-75311/weather_data_analyzer/src/analyzer.py/analyzer.py
@@ -0,0 +1,8 @@
+# TODO: Complete the function to read and process the weather data
+
+def analyze_weather_data(file_path):
+    # Your code here
+    pass
+
+if __name__ == '__main__':
+    analyze_weather_data('../data/weather_data.csv')
