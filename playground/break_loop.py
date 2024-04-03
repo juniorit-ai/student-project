193542a1505ebd626a6c6d0ce6bcd5ca1f146ac6
ezhomelabs <haiwei.he@gmail.com>
2024-04-03 07:22:35 +0000

    add session-53216

---
+++ playground/break_loop.py
@@ -0,0 +1,6 @@
+numbers_list = [1, 3, 5, 7, 9]
+for number in numbers_list:
+    if number == 5:
+        print('Number 5 found!')
+        break
+    print(number)
---
+++ playground/nested_loops.py
@@ -0,0 +1,4 @@
+two_dim_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for row in two_dim_array:
+    for item in row:
+        print(item)
