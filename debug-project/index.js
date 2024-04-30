0c9ca049e22e26889964d4b8983875a44a5d48ae
ezhomelabs <haiwei.he@gmail.com>
2024-04-30 11:39:28 +0000

    add session-75070

---
+++ debug-project/index.js
@@ -0,0 +1,4 @@
+var sumArray = require('./utilities');
+var numbers = [1, 2, 3, 4, 5];
+var result = sumArray(numbers);
+console.log('Sum of array elements is: ' + result); // Should output 'Sum of array elements is: 15'
---
+++ debug-project/main.js
@@ -0,0 +1,6 @@
+var result = 'Result: ';
+var numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(number) {
+  result += number;
+});
+console.log(result); // Should output 'Result: 12345'
