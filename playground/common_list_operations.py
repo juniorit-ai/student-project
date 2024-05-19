ab80da17a5312e90266b76f1f911d5ed12bb7b6b
ezhomelabs <haiwei.he@gmail.com>
2024-05-19 14:02:39 +1000

    add session-64643

---
+++ playground/common_list_operations.py
@@ -0,0 +1,6 @@
+# Common list operations
+my_list = [1, 2, 3]
+my_list.append(4)
+my_list.remove(2)
+print(my_list[0])
+print(my_list)
---
+++ playground/creating_initializing_lists.py
@@ -0,0 +1,5 @@
+# Creating and initializing lists
+empty_list = []
+predefined_list = [10, 20, 30]
+print(empty_list)
+print(predefined_list)
