bcbe318de053b4b33c47841f791b089f116756db
ezhomelabs <haiwei.he@gmail.com>
2024-05-24 12:36:53 +1000

    add session-70353

---
+++ list-handling-practice/access_elements.py
@@ -0,0 +1,7 @@
+
+# Accessing elements in the list
+fruits = ["apple", "banana", "cherry"]
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+print("First fruit:", first_fruit)
+print("Second fruit:", second_fruit)
---
+++ list-handling-practice/modify_list.py
@@ -0,0 +1,5 @@
+
+# Modifying elements in the list
+fruits = ["apple", "banana", "cherry"]
+fruits[1] = "blueberry"
+print("Modified Fruits:", fruits)
