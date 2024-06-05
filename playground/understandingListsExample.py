dbed0bee0077e8b75db11e5d289af5a673cb123e
ezhomelabs <haiwei.he@gmail.com>
2024-06-05 18:39:14 +1000

    add session-50358

---
+++ playground/understandingListsExample.py
@@ -0,0 +1,16 @@
+
+                        # Creating a list
+                        my_list = [1, 2, 3, 4, 5]
+                        print('Created list:', my_list)
+
+                        # Adding elements to the list
+                        my_list.append(6)
+                        print('List after adding element:', my_list)
+
+                        # Removing elements from the list
+                        my_list.remove(3)
+                        print('List after removing element:', my_list)
+
+                        # Accessing elements in the list
+                        print('Accessing element at index 2:', my_list[2])
+                    
