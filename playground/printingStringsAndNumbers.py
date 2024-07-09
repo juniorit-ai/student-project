5ad33dd9412ff3dfae346f3e14c313c1a996db24
ezhomelabs <haiwei.he@gmail.com>
2024-07-09 13:08:30 +1000

    Update printingStringsAndNumbers.py

--- playground/printingStringsAndNumbers.py
+++ playground/printingStringsAndNumbers.py
@@ -1,16 +1,16 @@
 
-                        # Printing a single string
-                        print('Hello, World!')
+# Printing a single string
+print('Hello, World!')
 
-                        # Printing a number
-                        print(42)
+# Printing a number
+print(42)
 
-                        # Printing multiple values
-                        print('The answer is:', 42)
+# Printing multiple values
+print('The answer is:', 42)
 
-                        # Combining strings and numbers using concatenation
-                        print('The answer is: ' + str(42))
+# Combining strings and numbers using concatenation
+print('The answer is: ' + str(42))
 
-                        # Using the format method to combine strings and numbers
-                        print('The answer is: {}'.format(42))
+# Using the format method to combine strings and numbers
+print('The answer is: {}'.format(42))
                     
